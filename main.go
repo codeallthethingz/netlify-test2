b05738715b4f326660753cb69cbc25e319478a25
codeallthethingz <howdoicontactwill@gmail.com>
2020-08-16 15:33:00 -0700

    env

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -10,6 +11,7 @@ import (
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	body := request.Body
 	fmt.Println(body)
+	fmt.Println("env:", os.Getenv("VARIABLE_ONE"))
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello AWS Lambda and Netlify",
